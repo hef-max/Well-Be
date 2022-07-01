dc5d3737c7ff92e345e6497e6aff34f9122e22da
hef-max <hefrykun10@gmail.com>
2022-07-01 08:54:02 +0700

    change a sett

--- wellb/wellb/settings.py
+++ wellb/wellb/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-5tqp@=790inef(_m))2fty)%@wzos!)ji17$y-sf36*sqnu^f%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['herokudjangoapp.herokuapp.com']
 
